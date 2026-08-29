ceabc65136bd8c47aa6bd59b5578c373c88f1a22
Buddhajyoti Roy <96861075+donuts420@users.noreply.github.com>
2026-08-29 16:53:09 +0530

    Update render_context.py

--- src/gh_space_shooter/game/render_context.py
+++ src/gh_space_shooter/game/render_context.py
@@ -51,6 +51,6 @@ class RenderContext:
             background_color=(13, 17, 23),
             grid_color=(22, 27, 34),
             enemy_colors={1: (0, 109, 50), 2: (38, 166, 65), 3: (57, 211, 83), 4: (87, 242, 135)},
-            ship_color=(240, 171, 252),
+            ship_color=(216, 180, 254),
             bullet_color=(216, 180, 254),
         )
